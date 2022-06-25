17a8c44d3e88c31d1a928430ebe2d776ebed2ff0
dhhan0430 <dhhan0430@gmail.com>
2022-06-25 21:22:11 +0900

    자바 스프링 프로젝트 RealEstimate : 초기 세팅

---
+++ MyProject/RealEstimate/src/main/java/dh/realestimate/RealEstimateApplication.java
@@ -0,0 +1,13 @@
+package dh.realestimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstimateApplication.class, args);
+	}
+
+}
---
+++ MyProject/RealEstimate/src/test/java/dh/realestimate/RealEstimateApplicationTests.java
@@ -0,0 +1,13 @@
+package dh.realestimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
